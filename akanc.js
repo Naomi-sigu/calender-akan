d65410eb2e1a10a3ee2cb64268fc0ede125298c1
Naomi <siguneema@gmail.com>
2019-07-16 17:15:06 +0300

    add variable

--- akanc.js
+++ akanc.js
@@ -1,28 +1,27 @@
 function input() {
-  var century = document.getElementById("century").value;
-  var birthYear=document.getElementById("birthyear").value;
-  var birthmonth=document.getElementById("birthmonth").value;
-  var birthdate=document.getElementById("birthdate").value;
+  var BirthYear=document.getElementById("birthyear").value;
+  var BirthMonth=document.getElementById("birthmonth").value;
+  var BirthDate=document.getElementById("birthdate").value;
 
-  var gender0=document.getElementById("gender0").value;
-  var gender1=document.getElementById("gender1").value;
+  var gender0=document.getElementById("gender").value;
+  var gender1=document.getElementById("gender").value;
 
-  var females=["Akosua","Adwoa","Abenna","Akua","Yaa","Afua","Ama"];
-  var males=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+  var Females=["Akosua","Adwoa","Abenna","Akua","Yaa","Afua","Ama"];
+  var Males=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
   var DD=["Sunday","Monday","Tuesday","wednesday","Thurday","Friday","Saturday"];
 
-  var dayOfBirth=parseInt(((century/4)-2*century-1)+((5*birthYear/4))+((26*(birthmonth+1)/10))+birthdate)% 7;
+  var DayOfBirth=parseInt(((century/4)-2*century-1)+((5*birthYear/4))+((26*(birthmonth+1)/10))+birthdate)% 7;
 
   if (gender0.checked === "male") {
-       var Mname = males[dayOfBirth - 1];
-       var day = DD[dayofBirth -1];
+       var Mname = Males[DayOfBirth - 1];
+       var day = DD[DayofBirth -1];
 
        document.getElementById('name').innerHTML= "You were born on" + day + "Your name is"+ Mname;
 
        }
        else if (gender1.checked === "female") {
-           var Fname = females[dayOfBirth -1];
-           var day = DD[dayOfBirth - 1];
+           var Fname = Females[DayOfBirth -1];
+           var day = DD[DayOfBirth - 1];
             document.getElementById('name').innerHTML= "You were born on" + day + "Your name is"+ Fname;
        }
 
